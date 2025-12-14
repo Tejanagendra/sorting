23b05da93e2c8a7973fcb0079414b5b4c436b204
Tejanagendra <tejawarrior150@gmail.com>
2025-12-14 08:24:56 +0530

    ~                                                                                                              ~                                                                                                              .git/COMMIT_EDITMSG [unix] (08:24 14/12/2025)                                                           4,1 All ?#    # Added gitignore and updated bubble sort implementation

--- bubble.java
+++ bubble.java
@@ -6,7 +6,6 @@ public class bubble {
         int[] arr= {3,2,1};
         int n=arr.length-1;
         for(int i=n;i>=0;i--){
-            
             for(int j=1;j<=i;j++){
                 if(arr[j-1]>arr[j]){
                     int temp =arr[j-1];
